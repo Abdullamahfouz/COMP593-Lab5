a9733a6d8fc15aa88b8e362cbae25bce36c3320a
Abdullamahfouz <abdullah.mahfouz@flemingcollege.ca>
2023-02-22 10:26:17 -0500

    Update mainscript.py

--- mainscript.py
+++ mainscript.py
@@ -21,7 +21,7 @@ def get_pokemon_info(pokemon_name):
     url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}/'
     response = requests.get(url)
     if response.status_code != 200:
-        print(f"Failed to fetch Pokémon information for {pokemon_name}.")
+        print(f"Failed to Pokémon information for {pokemon_name}.")
         sys.exit(1)
     return response.json()
 
